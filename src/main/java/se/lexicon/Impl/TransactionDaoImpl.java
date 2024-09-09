08e20d82accc8e00a8391dfe66540abcbd8900be
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-09 12:48:49 +0200

    Implemented

--- src/main/java/se/lexicon/Impl/TransactionDaoImpl.java
+++ src/main/java/se/lexicon/Impl/TransactionDaoImpl.java
@@ -1,4 +1,38 @@
 package se.lexicon.Impl;
 
-public class TransactionDaoImpl {
+import se.lexicon.Dao.TransactionDao;
+import se.lexicon.model.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class TransactionDaoImpl implements TransactionDao {
+
+    private List<Transaction> storage;
+
+    public TransactionDaoImpl() {
+        this.storage = new ArrayList<>();
+    }
+
+    @Override
+    public Transaction createTransaction(Transaction transaction) {
+        storage.add(transaction);
+        return transaction;
+    }
+
+    @Override
+    public Optional<Transaction> findById(String id) {
+        return storage.stream()
+                .filter(transaction -> transaction.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public List<Transaction> findTransactionByWalletId(String walletId) {
+        return storage.stream()
+                .filter(transaction -> transaction.getWalletId().equals(walletId))
+                .collect(Collectors.toList());
+    }
 }
