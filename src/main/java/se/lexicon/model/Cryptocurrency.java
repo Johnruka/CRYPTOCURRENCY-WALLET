f6f9382f40094d45fd912233cd6462296fe2a092
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-09 12:04:52 +0200

    Modified and implemented

--- src/main/java/se/lexicon/model/Cryptocurrency.java
+++ src/main/java/se/lexicon/model/Cryptocurrency.java
@@ -1,4 +1,16 @@
 package se.lexicon.model;
 
-public class Cryptocurrency {
+public enum Cryptocurrency {
+
+    BTC("Bitcoin"), ETH("Ethereum"), USDT("TetherUs"), BNB("BNB");
+
+    private final String name;
+
+    Cryptocurrency(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
