a7df1907ea980b5649aaad264616460138a5360d
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-09 12:19:39 +0200

    Created

---
+++ src/main/java/se/lexicon/Dao/WalletDao.java
@@ -0,0 +1,13 @@
+package se.lexicon.Dao;
+
+import se.lexicon.model.Wallet;
+
+import java.util.Optional;
+
+public interface WalletDao {
+
+    Wallet createWallet(Wallet wallet);
+
+    Optional<Wallet> findWalletById(String id);
+    //...
+}
