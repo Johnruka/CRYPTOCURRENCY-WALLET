7282d1331cf9aa437c7bc50e2724ce0a0f730517
John Baptist Rukarugahieguru <rukaruga@yahoo.co.uk>
2024-09-09 13:15:55 +0200

    Created

--- src/main/java/se/lexicon/service/TransactionManagement.java
+++ src/main/java/se/lexicon/service/TransactionManagement.java
@@ -1,4 +1,16 @@
 package se.lexicon.service;
 
+import se.lexicon.model.Cryptocurrency;
+import se.lexicon.model.Transaction;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public interface TransactionManagement {
+
+    Transaction createDepositTransaction(String walletId, Cryptocurrency cryptocurrency, BigDecimal amount, String description);
+
+    Transaction createWithdrawTransaction(String walletId, Cryptocurrency cryptocurrency, BigDecimal amount, String description);
+
+    List<Transaction> getTransactionsByWalletId(String walletId);
 }
--- src/main/java/se/lexicon/service/WalletManagement.java
+++ src/main/java/se/lexicon/service/WalletManagement.java
@@ -1,4 +1,10 @@
 package se.lexicon.service;
 
+import se.lexicon.model.Wallet;
+
 public interface WalletManagement {
+
+    Wallet create(String walletName);
+
+    Wallet getById(String id);
 }
